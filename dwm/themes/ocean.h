13dabc7bb2951356bd0075b74c909a37d84ba464
Sarbagya Nepal <chain@pop-os.localdomain>
2022-08-23 17:37:32 +0545

    Adding my suckless configs

---
+++ dwm/themes/ocean.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#0F111A";
+static const char col_gray2[]       = "#2c3039";
+static const char col_gray3[]       = "#E5E9F0";
+static const char col_gray4[]       = "#E5E9F0";
+static const char col_gray5[]       = "#8fa1b3";
+static const char col_cyan[]        = "#65737e";
